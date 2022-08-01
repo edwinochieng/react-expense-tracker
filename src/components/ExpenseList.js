8c564bcfc28dec1cd059ac54d3f5e023d35c26b6
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-08-01 20:58:20 +0300

    add new transactions

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React from "react";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';;
 
-function ExpenseList(props) {
+function ExpenseList({ transaction }) {
+  const sign = transaction.cost < 0 ? "-" : "+";
   return (
-        <li className='border border-solid border-violet-300 rounded-md my-3 h-7 w-80 px-3 text-sm flex justify-between items-center' >
-            {props.name}
-            <div >
-            <span>
-                ${props.cost}
-            </span>
-            </div>
-           
-        </li>
+    <div className="relative">
+<li
+      className={`border border-solid border-violet-300 rounded-md my-3 mx-5 h-7 w-80 px-2 text-sm flex justify-between items-center ${
+        transaction.cost < 0
+          ? "border-r-4 border-r-red-600"
+          : "border-r-4 border-r-green-500"
+      }`}
+    > 
+      {transaction.name}
+      <div>
+        <span>
+          {sign} ${Math.abs(transaction.cost)}
+        </span>
+      </div>
+      <button className="absolute left-0 h-6 "><DeleteForeverIcon sx = {{fontSize :16}}/></button>
+    </li>
+    </div>
     
-  )
+  );
 }
 
-export default ExpenseList
+export default ExpenseList;
--- src/components/NewTransaction.js
+++ src/components/NewTransaction.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewTransaction() {
+  const [title,setTitle] = useState("");
+  const [amount,setAmount] = useState(0);
   return (
     <div className="mt-4 font-quicksand ">
       <h1 className="pb-2 text-lg">Add new transacton </h1>
 
       <form className="border border-solid border-1 border-violet-200 rounded-lg h-48 w-80 pl-3">
-        <div >
+        <div>
           <div className="py-1">
             <label className="text-sm">Title</label>
           </div>
@@ -16,20 +18,33 @@ function NewTransaction() {
               className="h-7 w-72 rounded-md pl-2 text-sm border border-solid border-violet-300 focus:outline-none"
               type="text"
               placeholder="Enter title.."
+              value ={title}
+              onChange ={(event) => setTitle(event.target.value)}
+
             />
           </div>
 
           <div className="py-1">
             <label className="text-sm">Amount</label>
+            <h1 className="text-xs">(- for expense, + for income)</h1>
           </div>
 
           <div>
-            <input className="h-7 w-72 rounded-md pl-2 text-sm border border-solid border-violet-300 focus:outline-none" type="number" placeholder="Enter amount.." />
+            <input
+              className="h-7 w-72 rounded-md pl-2 text-sm border border-solid border-violet-300 focus:outline-none"
+              type="number"
+              placeholder="Enter amount.."
+              value ={amount}
+              onChange ={(event) => setAmount(event.target.value)}
+
+            />
           </div>
         </div>
-      
+
         <div className="mt-4">
-            <button className="h-8 w-72 border border-solid rounded-md bg-violet-500 items-center text-white">Add transacton</button>
+          <button className="h-8 w-72 border border-solid rounded-md bg-violet-500 items-center text-white">
+            Add transacton
+          </button>
         </div>
       </form>
     </div>
