a559966f18be6a25260c6530e25c14e988de1bb1
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-12-19 22:28:32 +0300

    format

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,20 @@ import NewTransaction from "./components/NewTransaction";
 import { GlobalProvider } from "./context/Globalcontext";
 function App() {
   return (
-    <div className="h-screen py-10 bg-violet-400 grid justify-items-center">
-      <div className="flex flex-col items-center rounded-3xl bg-zinc-100 border border-solid border-3 border-gray-100 w-96 pt-5">
-        <h1 className="font-bold font-quicksand pb-6 text-xl">
+    <div className='h-screen bg-violet-400 grid place-items-center'>
+      <div className='flex flex-col items-center rounded-3xl bg-zinc-100 border border-solid border-3 border-gray-100 w-96 pt-5'>
+        <h1 className='font-bold font-quicksand pb-6 text-xl'>
           Expense Tracker
         </h1>
         <GlobalProvider>
-        <Remaining />
-        <div className="flex flex-row mt-6">
-          <Budget />
-          <Spent />
-        </div>
-        <History />
-        <NewTransaction />
+          <Remaining />
+          <div className='flex flex-row mt-6'>
+            <Budget />
+            <Spent />
+          </div>
+          <History />
+          <NewTransaction />
         </GlobalProvider>
-       
       </div>
     </div>
   );
--- client/src/components/NewTransaction.js
+++ client/src/components/NewTransaction.js
@@ -1,63 +1,64 @@
 import React, { useContext, useState } from "react";
-import {GlobalContext} from "../context/Globalcontext";
+import { GlobalContext } from "../context/Globalcontext";
 
 function NewTransaction() {
-  const [name,setName] = useState("");
-  const [cost,setCost] = useState();
-  const {addTransaction} = useContext(GlobalContext)
-  function onSubmit(event){
-     event.preventDefault();
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState();
+  const { addTransaction } = useContext(GlobalContext);
+  function onSubmit(event) {
+    event.preventDefault();
 
-     const newTransaction = {
-      id: Math.floor(Math.random() *1000000),
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000000),
       name,
-      cost : +cost
-     }
-     
-    addTransaction(newTransaction)
-     
-    console.log(newTransaction)
+      cost: +cost,
+    };
+
+    addTransaction(newTransaction);
+
+    console.log(newTransaction);
   }
   return (
-    <div className="mt-4 font-quicksand ">
-      <h1 className="pb-2 text-lg">Add new transacton </h1>
+    <div className='mt-4 mb-12 font-quicksand '>
+      <h1 className='pb-2 text-lg'>Add new transacton </h1>
 
-      <form onSubmit ={onSubmit} className="border border-solid border-1 border-violet-200 rounded-lg h-48 w-80 pl-3">
+      <form
+        onSubmit={onSubmit}
+        className='border border-solid border-1 border-violet-200 rounded-lg h-48 w-80 pl-3'
+      >
         <div>
-          <div className="py-1">
-            <label className="text-sm">Title</label>
+          <div className='py-1'>
+            <label className='text-sm'>Title</label>
           </div>
 
           <div>
             <input
-              className="h-7 w-72 rounded-md pl-2 text-sm border border-solid border-violet-300 focus:outline-none"
-              type="text"
-              placeholder="Enter title.."
-              value ={name}
-              onChange ={(event) => setName(event.target.value)}
-
+              className='h-7 w-72 rounded-md pl-2 text-sm border border-solid border-violet-300 focus:outline-none'
+              type='text'
+              placeholder='Enter title..'
+              value={name}
+              onChange={(event) => setName(event.target.value)}
             />
           </div>
 
-          <div className="py-1">
-            <label className="text-sm">Amount</label>
-            <h1 className="text-xs">(- for expense, + for income)</h1>
+          <div className='py-1'>
+            <label className='text-sm'>Amount</label>
+            <h1 className='text-xs'>(- for expense, + for income)</h1>
           </div>
 
           <div>
             <input
-              className="h-7 w-72 rounded-md pl-2 text-sm border border-solid border-violet-300 focus:outline-none"
-              type="number"
-              placeholder="Enter amount.."
-              value ={cost}
-              onChange ={(event) => setCost(event.target.value)}
-
+              className='h-7 w-72 rounded-md pl-2 text-sm border border-solid border-violet-300 focus:outline-none'
+              type='number'
+              placeholder='Enter amount..'
+              value={cost}
+              onChange={(event) => setCost(event.target.value)}
             />
           </div>
         </div>
 
-        <div className="mt-4">
-          <button className="h-8 w-72 border border-solid rounded-md bg-violet-500 items-center text-white">
+        <div className='mt-4'>
+          <button className='h-8 w-72 border border-solid rounded-md bg-violet-500 items-center text-white'>
             Add transacton
           </button>
         </div>
