3f5992fe80e18fdeefbebc671f2c4a1891a7f23e
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-12-19 22:28:14 +0300

    add mongodb uri

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connetDB = async () => {
-    try{
-        const conn = await mongoose.connect("mongodb://localhost:27017/expensetracker");
-         console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch(err) {
-
-        console.log(`Error: ${err.message}`);
-        process.exit(1);
-     
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
 module.exports = connetDB;
